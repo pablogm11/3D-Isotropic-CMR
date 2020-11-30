7e46eb90765c09b8ed7f5b1dc07c0b3ab162ae50
pablogm11 <pgm11vigo@gmail.com>
2020-11-30 12:07:28 +0100

    plot

--- artificialSlice.py
+++ artificialSlice.py
@@ -9,18 +9,32 @@ x = np.arange(0,384,10)
 y = np.arange(0,384,10)
 z = np.arange(0,160,10)
 xy,yx,zx = np.meshgrid(x,y,z)
-# xy = xy -384
-# yx = yx -384
-# zx = zx -160
+ori = np.array([259,5,219])
+normal = np.array([0.75,0,-0.75])
+new_zx = np.zeros(zx.shape)
+new_zx = new_zx == 1
+ci = 0
+for i in np.arange(0,384,10):
+        cj = 0
+        for j in np.arange(0,384,10):
+            ck = 0
+            for k in np.arange(0,160,10):
+                if  normal[0]*i + normal[1]*j + normal[2]*k - (normal[0]*ori[0] + normal[1]*ori[1] + normal[2]*ori[2]) == 0:
+                    new_zx[ci,cj,ck] = True
+                else:
+                    new_zx[ci,cj,ck] = False
+                ck = ck +1
+            cj = cj + 1
+        ci = ci + 1
 fig = plt.figure()
 ax = fig.add_subplot(111,projection = '3d')
-ax.scatter(xy,yx,zx)
+ax.scatter(xy,yx,zx,c = new_zx)
 ax.set_zlim([0,384])
 plt.xlabel("X")
 plt.ylabel("Y")
 ax.set_zlabel("Z")
 
-normal = np.array([0.75,0,-0.75])
+
 z_ax = np.array([0,0,1])
 #Angle btw vectors 22 
 v = np.cross(z_ax,normal)
@@ -30,7 +44,7 @@ I = np.identity (3)
 vx = np.array([[0, -v[2],v[1]],[v[2],0, -v[0]],[-v[1],v[0],0]])
 R = I + vx + np.matmul(vx,vx) * ((1-c)/(s**2))
 angle = np.arccos(c)*180/np.pi
-
+print(angle)
 xy_new = np.zeros(xy.shape)
 yx_new = np.zeros(yx.shape)
 zx_new = np.zeros(zx.shape)
